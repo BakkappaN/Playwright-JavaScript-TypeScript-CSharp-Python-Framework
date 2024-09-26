b3715b78e9767bd51373d2244fe54fe221cb6299
BakkappaN <bakkappac@gmail.com>
2024-09-26 19:16:13 +0530

    Playwright typescript framework

--- Playwright-TypeScript/playwright.config.ts
+++ Playwright-TypeScript/playwright.config.ts
@@ -17,12 +17,13 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'never' }],
     // ['allure-playwright'],
     ['junit', { outputFile: './e2e-junit-results.xml' }],
+    [`./src/utils/test_listener.ts`],
   ],
 
   expect: {
