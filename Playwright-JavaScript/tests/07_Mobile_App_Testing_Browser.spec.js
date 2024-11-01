2838679765f60f57b571113e885fc1dbe4a4ead0
BakkappaN <bakkappac@gmail.com>
2024-11-01 12:10:23 +0530

    Playwright with Mobile App Testing

--- Playwright-JavaScript/tests/07_Mobile_App_Testing_Browser.spec.js
+++ Playwright-JavaScript/tests/07_Mobile_App_Testing_Browser.spec.js
@@ -5,7 +5,7 @@ test.beforeEach(async ({ page }) => {
     await page.goto(process.env.MOBILE_APP_URL.toString());
 })
 
-test('Validate testers talk playlist link', async ({ mobileAppPage }) => {
+test('Validate testers talk playlist link', { tag: '@MobileTest' }, async ({ mobileAppPage, page }) => {
 
     await test.step('Search with keywords', async () => {
         await page.screenshot({ path: './mobile_app_test_homepage.png' });
@@ -13,7 +13,7 @@ test('Validate testers talk playlist link', async ({ mobileAppPage }) => {
     })
 
     await test.step('Validate Playwright by Testers Talk link', async () => {
-        await mobileAppPage.validateTestersTalkLink();        
+        await mobileAppPage.validateTestersTalkLink();
         await page.screenshot({ path: './mobile_app_test_resultspage.png' });
     });
 })
